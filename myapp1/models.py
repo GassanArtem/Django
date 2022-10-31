3b08dccd45dcf9854e62f7eb2166625108fdc595
Artem <artemgassan@yandex.ru>
2022-10-31 19:44:00 +0300

    start project

---
+++ myapp1/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Worked(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    second_name = models.CharField(max_length=35, blank=False)
+    salary = models.IntegerField(default=0)
