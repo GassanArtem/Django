bb7b125d5f385bc443b4d3bc94a2b9448c1d00aa
Artem <artemgassan@yandex.ru>
2022-10-31 20:19:18 +0300

    Fix bag for work repository

--- myapp1/models.py
+++ myapp1/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Worked(models.Model):
+class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
